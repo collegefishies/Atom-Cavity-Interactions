g=33. fix bug in g calculation.

interaction.py
@@ -7,8 +7,8 @@ SAVE_ARRAYS = False
 
 GAMMA_SI = 180e3/2/np.pi
 KAPPA_SI = 500E3/2/np.pi
-ETA = 4
-G_SI = 2*np.pi*ETA*np.sqrt(GAMMA_SI*KAPPA_SI)/4
+ETA = 20
+G_SI = 2*np.pi*np.sqrt(4*ETA*GAMMA_SI*KAPPA_SI)
 # OMEGA_SI = 2*np.pi*5e6
 OMEGA_SI=10*KAPPA_SI
 LAMBDA_SI = 0.1*KAPPA_SI

interaction_picture_batch_semaphore.py
@@ -31,7 +31,7 @@ OMEGA_SI = 10*KAPPA_SI
 # LAMBDA_SI = 0.1*KAPPA_SI
 
 #parameters to test
-detuning_list = np.arange(-3*KAPPA_SI, 3*KAPPA_SI, KAPPA_SI/5) - OMEGA_SI
+detuning_list = np.arange(-2.1*OMEGA_SI, +OMEGA_SI, KAPPA_SI/5)
 
 number_of_params = len(detuning_list)
 if not args.onlycompile: